73c55b24a21410935145b1f562a3c8f1155c6477
Bulgarie <95614992+Bulgarie@users.noreply.github.com>
2021-12-06 10:05:47 +0100

    Create bobo.c

---
+++ bobo.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int taill= N;
+    int tab [N],cb,Max,Min,med;
+    float moy;
+    
+    printf("Saisir vos &d valeurs entiers du tableau (la &d ieme saisir '0') \n",N, N+1);
+    for (cb=0 ; cb<taille; cb++) {printf ("&d" ,tab [cb]);}
+    printf ("\n") ;
+    
+    printf ("maximum &d \n ", Max) ;
+    printf("minimum &d \n ",Min) ;
+    printf("moyenne &.2f \n ", moy) ;
+    printf(mediane &d \n ", med) ;
+    
+    
+    
+    
+
+    return 0;
+}
